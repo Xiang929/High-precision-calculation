e1aead187a00ee9d01b02eb5875d5cc6ff301f5e
Xiang <XiangCZ929@gmail.com>
2018-04-07 16:59:56 +0800

    Update infint.hpp

--- infint.hpp
+++ infint.hpp
@@ -39,4 +39,60 @@ public:
 private:
   string value;
 };
+#endif#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#ifndef _INFINT_HPP_
+#define _INFINT_HPP_
+
+using namespace std;
+
+class Infint
+{
+public:
+  Infint();
+  Infint(long long value);
+  Infint(string value);
+  Infint operator+(string number);
+  Infint operator-(string number);
+  Infint operator*(string number);
+  Infint &operator+=(string number);
+  Infint &operator-=(string number);
+  Infint &operator*=(string number);
+  Infint &operator=(string number);
+  Infint operator+(long long number);
+  Infint operator-(long long number);
+  Infint operator*(long long number);
+  Infint operator+(Infint number);
+  Infint operator-(Infint number);
+  Infint operator*(Infint number);
+  Infint &operator+=(long long number);
+  Infint &operator-=(long long number);
+  Infint &operator*=(long long number);
+  Infint &operator=(long long number);
+  Infint &operator+=(Infint number);
+  Infint &operator-=(Infint number);
+  Infint &operator*=(Infint number);
+  Infint &operator++(int);
+  Infint &operator--(int);
+  Infint &operator++();
+  Infint &operator--();
+  string minimalArray(string a, string b);
+  string maximalArray(string a, string b);
+  friend Infint operator+(long long number1, Infint number2);
+  friend Infint operator+(string number1, Infint number2);
+  friend Infint operator-(long long number1, Infint number2);
+  friend Infint operator-(string number1, Infint number2);
+  friend Infint operator*(long long number1, Infint number2);
+  friend Infint operator*(string number1, Infint number2);
+  friend ostream &operator<<(std::ostream &os, const Infint &cal_result);
+
+private:
+  string value;
+};
+
+void myAbs(string &number);
+bool isNegtiva(string number);
+int myAtoi(char ch);
 #endif
